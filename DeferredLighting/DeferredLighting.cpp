81222064b26ee3fe932bb0d33f8b70e810b32b45
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2012-08-01 18:27:13 +0200

    Changed documentation format

--- DeferredLighting/DeferredLighting.cpp
+++ DeferredLighting/DeferredLighting.cpp
@@ -1,50 +1,67 @@
-//
-// AUTHOR:	Christian Magnerfelt
-// DATE:	2012.05.12
-// Description: Main file for Deferred Lighting rendering process
-//
+ /*!
+ *	\brief		Deferred Lighting example
+ *	\details	Renders a simple example model using the multi-pass deferred lighting renderer
+ *	\file		DeferredLighting.cpp
+ *	\author		Christian Magnerfelt
+ *	\version	0.1
+ *	\date		2012.08.01
+ *	\copyright	GNU Public License
+ */
 
 #pragma once
 
-// OpenGL
-#include <GL/glew.h>
-#include <GL/freeglut.h>
+#include <GL/glew.h>				 //!< OpenGL EXT support
+#include <GL/freeglut.h>			 //!<  Window management library
+#include <Cg/cg.h>					 //!<  CG Run-Time Library API
+#include <Cg/cgGL.h>				 //!<  CGGL Run-Time Library API
+#include <iostream>					 //!<  Standard IO interface
 
-// Cg
-#include <Cg/cg.h>
-#include <Cg/cgGL.h>
-
-// STL
-#include <iostream>
-
-// Add libraries
+// Required libraries
 #pragma comment (lib , "cg.lib")
 #pragma comment (lib , "cgGL.lib")
 #pragma comment (lib , "freeglut.lib")
 #pragma comment (lib , "glew32.lib")
 
-///////// Global constants
+
 inline int getDefaultScreenWidth(){return 800; } const
 inline int getDefaultScreenHeight(){return 600; } const
 inline const char * getWindowTitle(){return "Deferred Lighting Demo"; } const
 
-//////// Global variables
 int currentScreenWidth = ::getDefaultScreenWidth();
 int currentScreenHeight = ::getDefaultScreenHeight();
-GLuint fboId;
-GLuint gBufferId;
-GLuint gBufferDepthId;
 
-///////// Forward declarations
+GLuint fboId;								//!< Id to our frame buffer object ( G-Buffer )
+GLuint gBufferId;							//!< ID of the G-Buffer part that contains normals and specular exponent at each pixel
+GLuint gBufferDepthId;						//!< ID of the G-Buffer part that contains the depth at each pixel
+
+CGcontext cgContext;
+CGprofile cgVertexProfile;
+CGprofile cgFragmentProfile;
+
+// Our vertex and fragment programs used for this demo
+CGprogram cgVertexProgram;
+CGprogram cgFragmentProgram;
+
+/*!
+ * Forward declarations
+ */
 void display();
 void reshape(int width, int height);
 void idle();
 void keyboard(unsigned char key, int x , int y);
 void initFrameBufferObject(int width, int height);
 void releaseFrameBufferObject();
+void shaderSetup();
 void cleanUp();
+void checkFramebufferStatus();
 void checkGLErrors(const char * action);
+void checkForCgError(const char * situation);
 
+/*!
+ * \brief Main enetry point for the program
+ * \details Initializes window and FBO. Enters glut main loop when initialization is complete.
+ * \return Return 0 after glut main loop has been been succesfully terminated
+ */
 int main(int argc, char * argv [])
 {
 	glutInit(&argc, argv);
@@ -52,7 +69,7 @@ int main(int argc, char * argv [])
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow(::getWindowTitle());
 	
-	// GlewInit
+	// Init extentions
 	GLenum glewErr = glewInit();
 	if (GLEW_OK != glewErr)
 	{
@@ -68,19 +85,39 @@ int main(int argc, char * argv [])
 	return 0;
 }
 
+/*!
+ * \brief Display callback
+ */
 void display()
 {
 	glutSwapBuffers();
 }
+
+/*!
+ * \brief Reshape callback
+ * \details Forces same width and height on resize
+ * \param width Not used, The new window width 
+ * \param height Not used, The new window height
+ */
 void reshape(int width, int height)
 {
-	// Force same width and height on resize
 	glutReshapeWindow(::currentScreenWidth,::currentScreenHeight); 
 }
+
+/*!
+ * \brief Idle callback
+ */
 void idle()
 {
 	glutPostRedisplay();
 }
+
+/*!
+ * \brief Keyboard callback
+ * \param key The id of the pressed key
+ * \param x Mouse x-position
+ * \param y Mouse y-position
+ */
 void keyboard(unsigned char key, int x , int y)
 {
 	switch(key)
@@ -93,12 +130,19 @@ void keyboard(unsigned char key, int x , int y)
 		break;
 	}
 }
+
+/*!
+ * \brief Initializes the frame buffer object in video memory
+ * \details The G-Buffer represented by the frame buffer object, consists of two textures. The first texture uses RGBA format 
+ * which is used to store the normals and specular emission. The RGB part holds the normals and the A part
+ * holds the spuclar emission. The second texture holds the depth of the scene. The texture format is depth using
+ * 24 bits. Note that texture will be upscaled to 32 bits in order to preserve the conistence wihtin the frame buffer object
+ * \param width In pixels, of the frame buffer object
+ * \param height In pixels, of the frame buffer object
+ */
 void initFrameBufferObject(int width, int height)
 {
 	// Generate and bind G-Buffer
-	// G-Buffer structure COLOR0:
-	// RGB	: View space normals
-	// A	: Specular emission
 	glGenTextures(1, &gBufferId);
 	glBindTexture(GL_TEXTURE_2D, gBufferId);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -107,13 +151,10 @@ void initFrameBufferObject(int width, int height)
 					GL_RGBA, GL_UNSIGNED_BYTE, 0); 
 	checkGLErrors("Generating G-Buffer texture RGBA");
 
-	// G-Buffer structure DEPTH:
-	// DEPTH24
 	glGenTextures(1, &gBufferDepthId);
 	glBindTexture(GL_TEXTURE_2D, gBufferDepthId);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	// 24 bit depth texture format
 	glTexImage2D   (GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, width, height, 0, 
 					GL_DEPTH_COMPONENT, GL_FLOAT, 0);
 	checkGLErrors("Generating G-Buffer texture DEPTH");
@@ -123,12 +164,11 @@ void initFrameBufferObject(int width, int height)
 	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboId);
 	checkGLErrors("Generating frame buffer object");
 
-	// Attach normal & specular buffer to frame buffer object
+	// Attach G-Buffer to FBO
 	glFramebufferTexture2DEXT  (GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT,
 								GL_TEXTURE_2D, gBufferId, 0);
 	checkGLErrors("Attach G-Buffer texture RGBA");
 
-	// Attach depth buffer to frame buffer object
 	glFramebufferTexture2DEXT  (GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT,
 								GL_TEXTURE_2D, gBufferDepthId, 0);
 	checkGLErrors("Attach G-Buffer texture Depth");
@@ -137,26 +177,76 @@ void initFrameBufferObject(int width, int height)
 	GLenum drawbuffers[] = {GL_COLOR_ATTACHMENT0_EXT}; 
 	glDrawBuffers(1, drawbuffers);
 	checkGLErrors("Enable drawing to attachments");
-	void checkFramebufferStatus(); 
+	// Check FBO status
+	void checkFramebufferStatus();
 }
+
+/*!
+ * \brief Frame buffer object clean up
+ */
 void releaseFrameBufferObject()
 {
 	glDeleteFramebuffersEXT(1,&fboId);
 	glDeleteTextures(1,&gBufferId);
 	glDeleteTextures(1,&gBufferDepthId);
 }
+
+/*!
+ * \brief 
+ */
+void shaderSetup()
+{
+	cgContext = cgCreateContext();
+	checkForCgError("Creating Context");
+	cgGLSetDebugMode(CG_FALSE);
+
+	// CG parameters will update immediately
+	cgSetParameterSettingMode(cgContext, CG_IMMEDIATE_PARAMETER_SETTING);
+
+	// Set up vertex profile & program
+	cgVertexProfile = cgGLGetLatestProfile(CG_GL_VERTEX);					
+	cgGLSetOptimalOptions(cgVertexProfile);
+	checkForCgError("Selecting Vertex Profile");
+
+	cgVertexProgram = cgCreateProgramFromFile(cgContext, CG_SOURCE, "DeferredLightingShaders.cg", 
+											  cgVertexProfile, "blinnPhongVTF", NULL);																
+	checkForCgError("Creating Vertex Program");
+
+	cgGLLoadProgram(cgVertexProgram);
+	checkForCgError("Loading Vertex Program");
+
+	// Set up fragment profile & program
+	cgFragmentProfile = cgGLGetLatestProfile(CG_GL_FRAGMENT);					
+	cgGLSetOptimalOptions(cgFragmentProfile);
+	checkForCgError("Selecting Fragment Profile");
+
+	cgFragmentProgram = cgCreateProgramFromFile(cgContext, CG_SOURCE, "DeferredLightingShaders.cg",											
+												cgVertexProfile, "blinnPhongFTB", NULL);																
+	checkForCgError("Creating Fragment Program");
+
+	cgGLLoadProgram(cgFragmentProgram);
+	checkForCgError("Loading Fragment Program");
+}
+
+/*!
+ * \brief 
+ */
 void cleanUp()
 {
 	releaseFrameBufferObject();
 }
-//////// Error handling function definitions
+
+/*!
+ * \brief Checks for OpenGL errors
+ * \param action OpenGL action that provoked the error
+ */
 void checkGLErrors(const char * action)
 {
 	GLenum status = glGetError();
 	switch(status)
 	{
 		case GL_NO_ERROR:
-			// OK 
+			// OK  
 			break;
 		case GL_INVALID_ENUM:
 			std::cerr << action 
@@ -176,13 +266,17 @@ void checkGLErrors(const char * action)
 	}
 
 }
+
+/*!
+ * \brief Checks that frame buffer object status is OK
+ */
 void checkFramebufferStatus()
 {
 	GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
 	switch(status)
 	{
 		case GL_FRAMEBUFFER_COMPLETE_EXT:
-			//GBuffer bind OK
+			// Frame buffer status OK
 			break;
 		case GL_FRAMEBUFFER_UNSUPPORTED_EXT:
 			std::cerr << "FBO Error : Framebuffer configuration not supported" 
@@ -195,7 +289,26 @@ void checkFramebufferStatus()
 			std::cerr << "FBO Error : Incomplete missing attachment" << std::endl;
 			break;
 		default:
-			//Error
 			std::cerr << "FBO Error : " << status << std::endl;
 	}
+}
+
+/*!
+ * \brief Check for errors in Cg runtime
+ * \param situation The context of the error
+ */
+void checkForCgError(const char * situation)
+{
+	CGerror error;
+	const char *string = cgGetLastErrorString(&error);
+
+	if (error != CG_NO_ERROR) 
+	{
+		std::cerr << ::GetTitleBarInfo << " : " << situation << " : " << string << std::endl;
+		if (error == CG_COMPILER_ERROR) 
+		{
+			std::cerr << cgGetLastListing(cgContext) << std::endl;
+		}
+		system("pause");
+  }
 }
